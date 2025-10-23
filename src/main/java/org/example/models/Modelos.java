3b69fa03d06261d50d12f1b5f99941f8c6592428
Luna Salazar <lunita0239@gmail.com>
2025-10-22 19:00:26 -0500

    cambios iniciales

--- src/main/java/org/example/models/Modelos.java
+++ src/main/java/org/example/models/Modelos.java
@@ -1,4 +1,20 @@
 package org.example.models;
 
 public class Modelos {
+    String Profesor;
+    String Alumno;
+    String Asignatura;
+    String Nota;
+    String Fecha;
+
+    public Modelos(String profesor, String alumno, String asignatura, String nota, String fecha) {
+        this.Profesor = profesor;
+        this.Alumno = alumno;
+        this.Asignatura = asignatura;
+        this.Nota = nota;
+        this.Fecha = fecha;
+    }
+
+    
+
 }
--- src/main/java/org/example/repository/Repositorios.java
+++ src/main/java/org/example/repository/Repositorios.java
@@ -1,4 +1,17 @@
 package org.example.repository;
 
 public class Repositorios {
+
+    public void guardar(Modelos modelos) {
+        
+    }
+
+    public void buscar(Modelos modelos) {
+        
+    }
+
+    public void eliminar(Modelos modelos) {
+        
+    }
+
 }
